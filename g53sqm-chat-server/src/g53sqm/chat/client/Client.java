962a5decad29767b15eb58b6e97fe8d794d12d8f
Vincent Hong <khfy6hzy@nottingham.edu.my>
2019-11-22 19:06:39 +0800

    Prototype for terminal Client

---
+++ g53sqm-chat-server/src/g53sqm/chat/client/Client.java
@@ -0,0 +1,163 @@
+package g53sqm.chat.client;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class Client {
+
+    private Socket socket = null;
+    private BufferedReader console = null; // client's terminal
+    private PrintWriter streamOut = null;  // send messages to server via socket output stream
+    private BufferedReader streamIn = null; // get responses from server via socket input stream
+    private ServerResponse serverResponse;
+    private ConsoleInput consoleInput;
+
+    // constructor for basic Client using computer terminal
+    public Client(String serverIp, int serverPort) {
+
+        startConnection(serverIp, serverPort);
+
+        // use threads for both server response and client input as IO operations are blocking calls
+        serverResponse = new ServerResponse();
+        Thread serverResponseThread = new Thread(serverResponse);
+        serverResponseThread.start();
+
+        consoleInput = new ConsoleInput();
+        Thread consoleInputThread = new Thread(consoleInput);
+        consoleInputThread.start();
+
+
+    }
+
+    public void startConnection(String serverIp, int serverPort) {
+
+        try{
+            // establish connection
+            socket = new Socket(serverIp,serverPort);
+
+            // set-up input and output streams
+            console = new BufferedReader(new InputStreamReader(System.in));
+            streamOut = new PrintWriter(socket.getOutputStream(),true);
+            streamIn = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        }
+        catch(UnknownHostException uhe){
+            System.out.println("Connection Error :" + uhe.getMessage());
+        }
+        catch(IOException ioe){
+            System.out.println("IO Error :" + ioe.getMessage());
+        }
+
+
+    }
+
+    // graceful exit of a Client
+    public void closeConnection(){
+
+        // clean up IO streams
+        try{
+            if(console!=null) console.close();
+            if(streamOut!=null) streamOut.close();
+            if(streamIn!=null) streamIn.close();
+        }
+        catch(IOException ioe){
+            System.out.println("Error closing IO streams: " + ioe.getMessage());
+        }
+
+
+        // finally we close the sockets
+        try{
+            if(socket!=null) socket.close();
+        }
+        catch(IOException ioe){
+            System.out.println("Error closing socket: " + ioe.getMessage());
+        }
+
+        // clean up threads
+        if(serverResponse!=null) serverResponse.shutdown();
+
+        if(consoleInput!=null) consoleInput.shutdown();
+
+
+    }
+
+    private class ServerResponse implements Runnable {
+
+        private volatile boolean running = true; // volatile to force thread to read from main memory
+
+        @Override
+        public void run() {
+
+            while(running){
+
+                if(streamIn != null) {
+
+                    try{
+                        String response = streamIn.readLine();
+                        System.out.println(response);
+                    }
+                    catch(IOException ioe){
+                        System.out.println("Error reading from server socket: " + ioe.getMessage());
+                    }
+
+                }
+
+            }
+        }
+
+        public void shutdown() {
+            running = false;
+        }
+    }
+
+    private class ConsoleInput implements Runnable {
+
+        private String input = "";
+        private volatile boolean running = true; // volatile to force thread to read from main memory
+
+        @Override
+        public void run() {
+
+            while(running){
+
+                if(streamOut != null && console != null) {
+
+                    try{
+                        input = console.readLine();
+                        streamOut.println(input);
+
+                        if(input.equals("QUIT")){
+                            closeConnection(); // try to exit gracefully after calling QUIT to prevent NullPointerException in server
+                        }
+                    }
+                    catch(IOException ioe){
+                        System.out.println("Error sending to server socket: " + ioe.getMessage());
+                    }
+
+                }
+
+            }
+        }
+
+        public void shutdown() {
+            running = false;
+        }
+    }
+
+    public static void main(String args[]){
+
+        Client client = null;
+
+        if(args.length !=2){
+            System.out.println("Usage: java Client ip port");
+        }
+        else{
+            client = new Client(args[0], Integer.parseInt(args[1]));
+        }
+
+    }
+
+}
